ae73ea7c7f20be4e8856ed8e0257226d405577b2
Chih-Han Liu <justgps@gmail.com>
2013-07-06 20:16:31 +0800

    This is the first version.

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('/index.html', function(err, data) {
-     if(err) response.send('500 Error.');
-     response.send(data);
+  fs.readFile('./index.html', function(err, data) {
+     if(err) response.send("500 error: " + err);
+     var buff = new Buffer(data);
+     //buff.toString(
+     response.send(buff);
   });
 });
 
